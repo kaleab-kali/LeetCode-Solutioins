90bbdba419bd8df9a1832c39f6c0c07027de8c69
kaleab Girma <86680782+kaleab-kali@users.noreply.github.com>
2022-10-30 17:34:24 +0000

    Time: 37 ms (97.52%), Space: 13.8 MB (68.33%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -5,3 +5,7 @@ class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
         return '0' if largest_num[0] == '0' else largest_num
+#     other solution
+        # nums = [str(x) for x in nums]
+        # nums.sort(cmp=lambda x, y: cmp(y+x, x+y))
+        # return ''.join(nums).lstrip('0') or '0'
