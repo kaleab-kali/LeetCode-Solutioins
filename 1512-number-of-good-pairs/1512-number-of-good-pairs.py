24da4818d0724b40bc9c051b97f7665364b42803
kaleab Girma <86680782+kaleab-kali@users.noreply.github.com>
2022-10-30 16:48:37 +0000

    Time: 72 ms (26.70%), Space: 13.8 MB (55.83%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,10 +1,26 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        nums.sort()
-        counter = 0
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] == nums[j] and i < j:
-                    counter += 1
+        my_count = 0
+        my_dict = {}
+        
+        for n in nums:
+            # Check to see if number has already been encountered
+            if n in my_dict:
+                # Increase count by the number of previous instances
+                my_count += my_dict[n]
+
+                # Increase the count of previous observation
+                my_dict[n] +=1
+            else:
+                # Store newly encountered number along with its count
+                my_dict[n] = 1
+                
+        return my_count
+#         nums.sort()
+#         counter = 0
+#         for i in range(len(nums)):
+#             for j in range(len(nums)):
+#                 if nums[i] == nums[j] and i < j:
+#                     counter += 1
                     
-        return counter
+#         return counter
