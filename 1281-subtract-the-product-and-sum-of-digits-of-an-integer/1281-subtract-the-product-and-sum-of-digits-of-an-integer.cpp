c8dc1e39195215bb7816644939063963e87782ca
kaleab Girma <86680782+kaleab-kali@users.noreply.github.com>
2022-10-31 17:43:32 +0000

    Time: 96 ms (50.76%), Space: 42.2 MB (50.25%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    const digits = Array.from(String(n), Number)
+    const sum = digits.reduce((a,b) => a+b)
+    const product = digits.reduce((a,b) => a*b)
+    return product-sum
+};
