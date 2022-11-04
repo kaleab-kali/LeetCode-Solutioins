c83ca8020e0bcfe1711310613fb28d30c9861b40
kaleab Girma <86680782+kaleab-kali@users.noreply.github.com>
2022-11-04 18:31:43 +0000

    Time: 42 ms (54.05%), Space: 5.8 MB (73.29%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,19 +1,7 @@
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        # if n <= 0:
-        #     return False
-        # if 1162261467 % n == 0:
-        #     return True
-        # else:
-        #     return False
-        # return 1162261467 % n == 0
-        if n== 0: 
-            return False
-        if n==1:
-            return True
-        
-        while(n%3 == 0):
-            n /= 3
-            
-        return n==1
-       
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 1) return n == 1;
+	    return n % 3 == 0 && isPowerOfThree(n / 3);
+    }
+};
