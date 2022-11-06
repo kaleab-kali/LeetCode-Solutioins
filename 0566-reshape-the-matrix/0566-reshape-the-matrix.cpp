18943e08b30724736d6026d996bdc1d90d3c32f1
kaleab Girma <86680782+kaleab-kali@users.noreply.github.com>
2022-11-06 20:23:40 +0000

    Time: 31 ms (13.92%), Space: 10.8 MB (41.27%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -1,7 +1,10 @@
-class Solution:
-    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        flat = sum(mat, [])
-        if len(flat) != r * c:
-            return mat
-        tuples = zip(*([iter(flat)] * c))
-        return map(list, tuples)
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size(), n = mat[0].size(), o = m * n;
+        if (r * c != o) return mat;
+        vector<vector<int>> res(r, vector<int>(c, 0));
+        for (int i = 0; i < o; i++) res[i / c][i % c] = mat[i / n][i % n];
+        return res;
+    }
+};
