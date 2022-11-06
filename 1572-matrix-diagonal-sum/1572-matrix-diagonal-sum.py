3d3301cab01f331089a14bc28517760991ea9221
kaleab Girma <86680782+kaleab-kali@users.noreply.github.com>
2022-11-06 20:15:59 +0000

    Time: 232 ms (58.48%), Space: 14.2 MB (56.19%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,16 +1,3 @@
-/**
- * @param {number[][]} mat
- * @return {number}
- */
-var diagonalSum = function(mat) {
-    let sum = 0
-  let j = mat[0].length - 1
-
-  mat.forEach((row, i) => {
-    if(i !== j - i)
-      sum += row[j - i]
-    sum += row[i]
-  })
-
-  return sum
-};
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        return sum(sum(r[j] for j in {i, len(r) - i - 1}) for i, r in enumerate(mat))
