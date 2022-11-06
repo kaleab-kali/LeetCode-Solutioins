4740306bd95e161518de8d02bc74aa1a70c253cf
kaleab Girma <86680782+kaleab-kali@users.noreply.github.com>
2022-11-06 20:18:22 +0000

    Time: 36 ms (33.23%), Space: 11.3 MB (11.17%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,3 +1,11 @@
-class Solution:
-    def diagonalSum(self, mat: List[List[int]]) -> int:
-        return sum(sum(r[j] for j in {i, len(r) - i - 1}) for i, r in enumerate(mat))
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size(),ans=0;
+        for(int i=0;i<n;i++){
+            if(i!=n-i-1) ans+=mat[i][i]+mat[i][n-i-1];
+            else ans+=mat[i][i];   
+        }
+        return ans;
+    }
+};
